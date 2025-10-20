998cb62a9d98f20d2e8276ac019d617c86df79ee
Gabby <GabbyVByers@gmail.com>
2025-10-19 20:01:08 -0700

    Add project files.

---
+++ main.cu
@@ -0,0 +1,24 @@
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main()
+{
+    sf::RenderWindow window;
+    window.create(sf::VideoMode({ 800, 800 }), "My Window");
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Blue);
+        window.display();
+    }
+
+    return 0;
+}
