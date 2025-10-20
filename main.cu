32e4e3923e9b8a6a7d0bbd482b19586cac6c5b6a
Gabby <GabbyVByers@gmail.com>
2025-10-19 20:24:16 -0700

    this bozo over here didn't know that assert statements are only invoked in debug mode (im the bozo)

--- main.cu
+++ main.cu
@@ -5,14 +5,35 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cassert>
+
 class CIFAR_DataSet {
+public:
     CIFAR_DataSet() {
+        std::vector<std::ifstream> CIFAR_binary_files;
+        CIFAR_binary_files.push_back(std::ifstream("CIFAR/data_batch_1.bin", std::ios::binary));
+        CIFAR_binary_files.push_back(std::ifstream("CIFAR/data_batch_2.bin", std::ios::binary));
+        CIFAR_binary_files.push_back(std::ifstream("CIFAR/data_batch_3.bin", std::ios::binary));
+        CIFAR_binary_files.push_back(std::ifstream("CIFAR/data_batch_4.bin", std::ios::binary));
+        CIFAR_binary_files.push_back(std::ifstream("CIFAR/data_batch_5.bin", std::ios::binary));
+
+        for (int i = 0; i < 5; i++) {
+            std::ifstream& file = CIFAR_binary_files[i];
+            assert(file.is_open());
+        }
 
     }
 };
 
 int main()
 {
+    
+    CIFAR_DataSet CIFAR;
+    
+    
     sf::RenderWindow window;
     window.create(sf::VideoMode({ 800, 800 }), "My Window");
 
